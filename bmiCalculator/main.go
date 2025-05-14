f02ab9cbb15162f24622a3e4a2b2df373385f20e
Artur <uber-alt-7@yandex.ru>
2025-05-15 02:37:48 +0300

    Калькулятор ИМТ готов!

--- bmiCalculator/main.go
+++ bmiCalculator/main.go
@@ -8,13 +8,28 @@ import (
 func main() {
 	// const bmiPower = 2
 	var userHeight, userWeight float64
+	var result string
 	fmt.Print("Введите рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите вес в килограммах: ")
 	fmt.Scan(&userWeight)
 	bmi := userWeight / math.Pow(userHeight/100, 2)
-	result := fmt.Sprintf("Ваш индекс массы тела: %.f", bmi)
-	fmt.Print("Ура! ", result)
+	if bmi < 16 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас выраженный дефицит массы тела.", bmi)
+	} else if bmi >= 16 && bmi < 18 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас недостаточная масса тела.", bmi)
+	} else if bmi >= 18 && bmi < 25 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас нормальный вес.", bmi)
+	} else if bmi >= 25 && bmi < 30 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас избыточная масса тела.", bmi)
+	} else if bmi >= 30 && bmi < 35 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас ожирение I степени.", bmi)
+	} else if bmi >= 35 && bmi < 40 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас ожирение I степени.", bmi)
+	} else if bmi >= 40 {
+		result = fmt.Sprintf("Ваш индекс массы тела: %.f.\nУ Вас ожирение III степени.", bmi)
+	}
+	fmt.Print(result)
 }
 
 /*
@@ -24,5 +39,14 @@ if <condition1> {
     ...
 } else {
 	...
+}
+
+switch variable {
+case 1:
+	<...>
+case 2, 3, 4:
+	<...>
+default:
+	<...>
 }
 */
